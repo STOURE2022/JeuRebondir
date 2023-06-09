124a88cc411b6e6ff20fbc19d28abcfdc3dae0e9
TOURE <toure@gmail.com>
2023-06-09 10:51:35 +0200

    Ajout rappel fonction et boucle de timing après les fonction

--- JeuRebondir.py
+++ JeuRebondir.py
@@ -73,3 +73,15 @@ tk.wm_attributes("-topmost", 1)
 canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
 canvas.pack()
 tk.update()
+
+raquette = Raquette(canvas, "black")
+balle = Balle(canvas, raquette, "red")
+
+
+while 1:
+    if balle.touche_bas == False:
+        balle.dessiner()
+        raquette.dessiner()
+    tk.update_idletasks()
+    tk.update()
+    time.sleep(0.04)
