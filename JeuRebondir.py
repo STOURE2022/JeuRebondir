f78071eff6adf03f00f0c3f1821f741622cc9fc0
TOURE <toure@gmail.com>
2023-06-09 09:53:48 +0200

    Ajout de ma classe balle à mon jeu

---
+++ JeuRebondir.py
@@ -0,0 +1,20 @@
+# Le jeu cavanas rebondir (balle et raquette)
+from tkinter import *
+import random
+import time
+
+
+# On va cr�er la class de la balle
+class Balle:
+    def __init__(self, canvas, raquette, couleur):
+        self.canvas = canvas
+        self.raquette = raquette
+        self.id = canvas.create_oval(10, 10, 25, 25, fill=couleur)
+        self.canvas.move(self.id, 245, 100)
+        departs = [-3, -2, -1, 1, 2, 3]
+        random.shuffle(departs)
+        self.x = departs[0]
+        self.y = -3
+        self.hauteur_canevas = self.canvas.winfo_height()
+        self.largeur_canevas = self.canvas.winfo_width()
+        self.touche_bas = False
